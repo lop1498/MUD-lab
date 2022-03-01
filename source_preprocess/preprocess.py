f7347192dd18bb7a1ac269e307426b3d9dca038b
Pol Garcia Recasens <polgarciarecasens@gmail.com>
2022-03-01 17:56:58 +0100

    add preprocess

--- source_preprocess/preprocess.py
+++ source_preprocess/preprocess.py
@@ -1,5 +1,5 @@
 import nltk
-
+import string
 
 #Tokenizer function. You can add here different preprocesses.
 def preprocess(sentence, labels):
@@ -14,6 +14,21 @@ def preprocess(sentence, labels):
     # Place your code here
     # Keep in mind that sentence splitting affectes the number of sentences
     # and therefore, you should replicate labels to match.
+
+    final_sentences = []
+    final_labels = []
+
+    for i in range(len(sentence)):
+        s = sentence[i].lower()
+        sentence_p = "".join([char for char in s if char not in string.punctuation])
+        words = nltk.word_tokenize(sentence_p)
+
+        porter = nltk.PorterStemmer()
+        stemmed = [porter.stem(word) for word in words]
+
+        final_sentences.append(stemmed)
+        final_labels.append([labels[i] for x in range(len(sentence[i]))])
+
     return sentence,labels
 
 
